2ad205adff13847119698f3c714d9b5fea3dcc87
Ramiro <ramiroacosta2213@gmail.com>
2022-11-19 18:49:22 -0300

    agrego pagina create

--- PIG_G3_22816/pages/views.py
+++ PIG_G3_22816/pages/views.py
@@ -1,6 +1,8 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Page
+from django.urls import reverse_lazy
 
 # Create your views here.
 class PageListView(ListView):   
@@ -9,3 +11,8 @@ class PageListView(ListView):
 class PageDetailView(DetailView):   
     model = Page
 
+
+class PageCreate(CreateView):
+    model = Page
+    fields = ['room', 'name', 'dni', 'entry', 'leave', 'price', 'reserve'] 
+    success_url= reverse_lazy ('pages:pages')
