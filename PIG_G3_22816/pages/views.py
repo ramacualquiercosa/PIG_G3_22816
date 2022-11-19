d43bc6b459aafbe8bcf0e6f6d8fd9804a35db684
Ramiro <ramiroacosta2213@gmail.com>
2022-11-19 19:33:05 -0300

    agrego update y delete

--- PIG_G3_22816/pages/views.py
+++ PIG_G3_22816/pages/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Page
 from django.urls import reverse_lazy
 
@@ -15,4 +15,16 @@ class PageDetailView(DetailView):
 class PageCreate(CreateView):
     model = Page
     fields = ['room', 'name', 'dni', 'entry', 'leave', 'price', 'reserve'] 
+    success_url= reverse_lazy ('pages:pages')
+
+class PageUpdate(UpdateView):
+    model = Page
+    fields = ['room', 'name', 'dni', 'entry', 'leave', 'price', 'reserve']
+    template_name_suffix = '_update_form'
+    
+    def get_success_url(self):
+        return reverse_lazy ('pages:update', args=[self.object.id]) +'?ok'
+
+class PageDelete(DeleteView):
+    model = Page
     success_url= reverse_lazy ('pages:pages')
